66d071f2f3c9d8fd5018c42e2d976f0d2533b1a1
eric-cc-su <eric-cc-su@users.noreply.github.com>
2020-12-13 00:43:00 -0500

    Complete HTML generation and index page replacement

--- update_books.sh
+++ update_books.sh
@@ -19,9 +19,30 @@ fi
 
 # Save data to temporary XML file and process XML
 XML_FILEPATH="tmp_gr_shelf.xml"
-if [ ! -z ENDPOINT ]; then
+INDEX_FILEPATH="src/index.html"
+HTML_FILEPATH=""
+
+if [ ! -z $ENDPOINT ]; then
     curl $ENDPOINT > $XML_FILEPATH
-    python3 parse_currently_reading.py $XML_FILEPATH
+    HTML_FILEPATH=$(python3 parse_currently_reading.py $XML_FILEPATH)
+    if [ $? -eq 0 ]; then
+        open -a "Google Chrome" $HTML_FILEPATH
+
+        # https://www.shellhacks.com/yes-no-bash-script-prompt-confirmation/
+        while true; do
+            read -p "Overwrite current index.html with auto-generated HTML? (y/n)" yn
+            case $yn in
+                [Yy]* )
+                    rm $INDEX_FILEPATH
+                    mv $HTML_FILEPATH $INDEX_FILEPATH
+                    break;;
+                [Nn]* )
+                    rm $HTML_FILEPATH
+                    break;;
+                * ) echo "Please answer yes or no.";;
+            esac
+        done
+    fi
 fi
 
 # Remove temporary XML file
